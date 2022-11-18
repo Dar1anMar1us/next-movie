483c1e1850ec58d8593ebed0490c7a19fbd4ea16
DarianChirca <darian.chirca@qmenta.com>
2022-11-18 13:41:15 +0100

    Add shared components

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import headerStyles from '../styles/Header.module.css'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className={headerStyles.title}>
+        <span>NextJS</span> Movies App
+      </h1>
+      <p className={headerStyles.description}>
+        This is a test project by Darian Chirca
+      </p>
+    </div>
+  )
+}
+
+export default Header
